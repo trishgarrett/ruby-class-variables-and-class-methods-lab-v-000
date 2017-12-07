e4c943ba875fac4a72ad5e1d17211c754e934b0f
Trisha Garrett <trisha.garrett@gmail.com>
2017-12-07 18:40:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,19 +35,13 @@ class Song
 
 def self.genre_count
   counts = Hash.new 0
-  words.each do |word|
-  counts[word] += 1
+  @@genres.collect do |genre|
+    if counts[genre]
+      counts[genre]  += 1
+    else
+      counts [genre] = 1
+    end
+  end
+  counts
 end
-counts
-end
-#   counts = Hash.new 0
-#   @@genres.collect do |genre|
-#     if counts[genre]
-#       counts[genre]  += 1
-#     else
-#       counts [genre] = 1
-#     end
-#   end
-#   counts
-# end
 end
